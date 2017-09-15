ded7949238d7e057ac51486ba2b0acd970217143
DragK <mlodyz.97@outlook.com>
2017-09-15 21:05:17 +0200

    Added TypeScript and ReactJS

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
-const TypeScript = require('typescript');
 const webpack = require('webpack');
 const path = require('path');
 
-const config = {
-  entry: './index.js',
+module.exports = {
+  entry: "./src/index.tsx",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filname: 'my-first-webpack.bundle.js'
+      filename: "bundle.js",
+      path: __dirname + "/dist"
   },
-  module: {
-    rules: [
-      {test: /\.text$/, use: 'raw-loader'}
-    ]
-  }
-}
 
-module.exports = config;
+  // Enable sourcemaps for debugging webpack's output.
+  devtool: "source-map",
+
+  resolve: {
+      // Add '.ts' and '.tsx' as resolvable extensions.
+      extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+
+  module: {
+      rules: [
+          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
+  },
+
+  // When importing a module whose path matches one of the following, just
+  // assume a corresponding global variable exists and use that instead.
+  // This is important because it allows us to avoid bundling all of our
+  // dependencies, which allows browsers to cache those libraries between builds.
+  externals: {
+      "react": "React",
+      "react-dom": "ReactDOM"
+  },
+};
