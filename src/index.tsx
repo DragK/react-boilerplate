8ea1773c4d247f3b5b32a95515a081ff99b805ae
H.M Murdock <mrdck@icloud.com>
2017-09-16 00:25:58 +0200

    ts-loader , gitignore , .babelrc

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {Hello} from './components/Hello';
 
+
 ReactDOM.render(
-  <Hello compiler="TypeScript" framework="ReactJS" />,
-  document.getElementById("example")
-)
+   <Hello compiler="TypeScript" framework="ReactJS" />,  
+  document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,70 @@
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const path = require('path');
+const webpack = require('webpack');
+
+
 
 module.exports = {
-  entry: "./src/index.tsx",
-  output: {
-      filename: "bundle.js",
-      path: __dirname + "/dist"
+    entry: './src/index.tsx',
+      output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.bundle.js'
   },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
 
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
 
-  resolve: {
-      // Add '.ts' and '.tsx' as resolvable extensions.
-      extensions: [".ts", ".tsx", ".js", ".json"]
-  },
+    module:{
+        rules: [
+            {
+                test: /\.(tsx|ts|js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader', 'ts-loader']
+            },
+         {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      }, 
+      {
+          test: /\.png$/,
+          use: [
+              {
+               loader: 'file-loader',
+               options: {
+                   name: '[path][hash].[ext]',
+                   outputPath: 'img/'
+                   
+               }
+              }
+          ]
+      }
 
-  module: {
-      rules: [
-          // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-          { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    
+    
+        
 
-          // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-          { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-      ]
-  },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
-      "react": "React",
-      "react-dom": "ReactDOM"
-  },
-};
+        ]
+    },
+    devServer:{
+        contentBase: path.join(__dirname, 'dist'),
+        stats: "errors-only",
+        compress: true,
+        port: process.env.PORT,
+        historyApiFallback: true,
+        headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+            "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        },
+    },
+    plugins: [new HtmlWebpackPlugin({
+         template: './src/index.html'
+    }),
+     new ExtractTextPlugin('style.css')
+    ]
+}
