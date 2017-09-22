2280725ed35233d59e0fdd16049befc292e432a4
DragK <mlodyz.97@outlook.com>
2017-09-22 23:23:36 +0200

    Types, routes

---
+++ src/routes/routes.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import {Router} from 'react-router-dom';
+
+export const ROUTES = {
+  index: "referenceToComponent"
+}
+
+let Routes = () => (
+  <Router>
+
+  </Router>
+)
+
+export default Routes;
