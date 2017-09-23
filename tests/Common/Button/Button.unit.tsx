77e0146bfd663236923cd977f72a5a0c24e4d577
H.M Murdock <mrdck@icloud.com>
2017-09-23 21:53:08 +0200

    snapshot issue fix

--- tests/Common/Button/Button.unit.tsx
+++ tests/Common/Button/Button.unit.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react'
 import { mount } from 'enzyme'
-import { Button } from 'Common'
+import { Button } from '../../../src/Common/index'
 
+describe('Button' , () => {
+    it('it renders button ' , () => {
+        const wrapper = mount(<Button label='test'/>)
+          expect(wrapper).toHaveLength(1)
+    })
 
-
-describe("Button" , () => {
-    it("it renders button" , () => {
-        const wrapper = mount(<Button />);
-          expect(wrapper).toMatchSnapshot();
+    it('it match with snapshot' , () => {
+        const wrapper = mount(<Button label='test'/>)
+        expect(wrapper).toMatchSnapshot()
     })
 })
 
